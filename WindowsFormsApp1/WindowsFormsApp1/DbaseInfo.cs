2d8c79570b9d219446f776933f255bd9a0460b8b
CrunchySea65 <egorliberty@gmail.com>
2017-04-27 19:57:19 +0300

    19.56
    Вся логика вынесена в отдельные классы

--- WindowsFormsApp1/WindowsFormsApp1/DbaseInfo.cs
+++ WindowsFormsApp1/WindowsFormsApp1/DbaseInfo.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-
 
 
 namespace WindowsFormsApp1
 {
     class DbaseInfo
     {
-        public string Server;// IP адрес БД
-        public string Database; // Имя БД
-        public string UserID;// Имя пользователя БД
-        public string Password;// Пароль пользователя БД
-        public string CharacterSet;// Кодировка Базы Данных
-        public uint Port;// Порт подключения к базе   
+        string Server;// IP адрес БД
+        string Database; // Имя БД
+        string UserID;// Имя пользователя БД
+        string Password;// Пароль пользователя БД
+        string CharacterSet;// Кодировка Базы Данных
+        uint Port;// Порт подключения к базе   
         public TableInfo[] tables;
+        //public static MySqlConnectionStringBuilder mysqlCSB = new MySqlConnectionStringBuilder();
+        public static MySqlConnection myConnection = new MySqlConnection();
+        MySqlCommand myCommand = new MySqlCommand();
+        public static DbaseInfo dbase = new DbaseInfo("db4free.net", "oblikvykladachiv", "guy123", "guy123", "cp1251", 3307);
 
         public DbaseInfo()
         {
@@ -26,7 +33,6 @@ namespace WindowsFormsApp1
             this.UserID = "UserID";
             this.Password = "Password";
         }
-
         public DbaseInfo(string server, string db, string user, string pass)
         {
             this.Server = server;
@@ -34,7 +40,6 @@ namespace WindowsFormsApp1
             this.UserID = user;
             this.Password = pass;
         }
-
         public DbaseInfo(string server, string db, string user, string pass, string charset,uint port)
         {
             this.Server = server;
@@ -44,12 +49,10 @@ namespace WindowsFormsApp1
             this.Port = port;
             this.CharacterSet = charset;
         }
-
         public void AddTables(TableInfo[] tables)
         {
             this.tables =tables;
         }
-
         public string Print()
         {
             string print = "";
@@ -63,6 +66,57 @@ namespace WindowsFormsApp1
             }
             return print;
         }
-
+        public MySqlConnectionStringBuilder DataConnect()//присваиваем значения для подключения к серверу
+        {
+            MySqlConnectionStringBuilder mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.Server = dbase.Server;
+            mysqlCSB.Database = dbase.Database;
+            mysqlCSB.UserID = dbase.UserID;
+            mysqlCSB.Password = dbase.Password;
+            mysqlCSB.CharacterSet = dbase.CharacterSet;
+            mysqlCSB.Port = dbase.Port;
+            return mysqlCSB;
+        }
+        public DataTable GetData(TableInfo table)
+        {
+            DataTable dataTable = new DataTable();//создаем таблицу чтобы передать ей значения
+               //присваиваем значения необходимые для подключения к базе(серверу)
+            AddTables(TableInfo.tables);
+            var temp = "";
+            for (int i = 0; i < table.cols.Length; i++)//преобразовуем информацию о колонках в строку
+            {
+                var j = ", ";
+                if (i == table.cols.Length - 1) { j = " "; }
+                temp += table.cols[i] + j;
+            }
+            //создаем запрос
+            string select = (@"select " + temp + " from " + table.name);
+            string queryString = select;
+            //MessageBox.Show(table.Print());//сообщение для отладки
+            //создаем соединение и получаем значения искомой таблицы
+            using (myConnection)
+            {
+                myConnection.ConnectionString = dbase.DataConnect().ConnectionString;
+                myCommand.CommandText =queryString;
+                myCommand.Connection = myConnection;
+                try
+                {
+                    myConnection.Open();
+                    using (MySqlDataReader dataReader = myCommand.ExecuteReader())
+                    {
+                        if (dataReader.HasRows)
+                        {
+                            dataTable.Load(dataReader);
+                        }
+                    }
+                    myConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            return dataTable;//получаем готовую таблицу
+        }
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/TableInfo.cs
+++ WindowsFormsApp1/WindowsFormsApp1/TableInfo.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp1
@@ -14,7 +18,13 @@ namespace WindowsFormsApp1
         public string[] cols;
         public static int count=0;
         public static TableInfo[] tables=new TableInfo[100]; //кол-во макс таблиц
-       
+        //добавляем данные о таблице name , cols
+        public static TableInfo oblik = new TableInfo("Oblik", "Id,PIB,Date,CameIn,ComeOut");
+        public static TableInfo vikladachy = new TableInfo("Vikladachy", "Id,PIB,Predmet,Kafedra");
+        public static TableInfo oblikStudent = new TableInfo("Oblik_Student", "PIB,_Group,SubGr,Predmet,Vykladach,Time,Data");
+        public static TableInfo rozklad = new TableInfo("Rozklad", "Kafedra,Predmet,Time,_Group,SubGr,Vykladach");
+        public static TableInfo studenty = new TableInfo("Studenty", "Id,PIB,_Group,SubGr,Kafedra,Telephone");
+
         public TableInfo()
         {
             name = "unknown";
@@ -22,7 +32,6 @@ namespace WindowsFormsApp1
             tables[count] = this;
             count++;   
         }
-
         public TableInfo(string name)
         {
             this.name = name;
@@ -30,7 +39,6 @@ namespace WindowsFormsApp1
             tables[count] = this;
             count++;
         }
-
         public TableInfo(string name,string cols)
         {
             this.name = name;
@@ -39,18 +47,39 @@ namespace WindowsFormsApp1
             tables[count] = this;
             count++;
         }
-
         public void SetName(string name)
         {
             this.name = name;
         }
-
         public void SetCols(string cols)
         {
             char[] sepchars = { ',' };
             this.cols = cols.Split(sepchars, StringSplitOptions.RemoveEmptyEntries);
         }
-
+        static public string[] Add_Tables()
+        {
+            //MessageBox.Show(TableInfo.count.ToString());
+            string[] print = new string[TableInfo.count];
+            for (int i = 0; i < TableInfo.count; i++)
+            {
+                print[i] = TableInfo.tables[i].name;
+            }
+            string[] arr = print;
+            return arr;
+            //tableSelect.Items.AddRange(arr);
+        }
+        static public void AddToControls(ComboBox combobox,DataGridView datagrid)
+        {
+            //combobox.Items.AddRange(Add_Tables());
+            int count = combobox.Items.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (combobox.SelectedItem.ToString() == combobox.Items[i].ToString())
+                {
+                    datagrid.DataSource = DbaseInfo.dbase.GetData(TableInfo.tables[i]); 
+                }
+            }
+        }
         public string Print()
         {
             var temp = "";
